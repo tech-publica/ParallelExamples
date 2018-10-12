7e099f61a6d823f4ca7d6ec9609a0ace76e1dbb9
Riccardo Audano <riccardo.at.work@gmail.com>
2018-10-12 08:43:25 +0200

    Add project files.

---
+++ ParallelExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParallelExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
