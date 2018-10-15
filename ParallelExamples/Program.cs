bc1687a327bb927e7d36be20f6e183545e9144aa
corsoNetFinsa <corsoNetFinsa@outlook.it>
2018-10-15 10:19:03 +0200

    example of what happens when you don t lock your bathrooms (Account)

--- ParallelExamples/Program.cs
+++ ParallelExamples/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ParallelExamples.ThreadExamples;
+using System;
+using System.Threading;
 
 namespace ParallelExamples
 {
@@ -6,7 +8,50 @@ namespace ParallelExamples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Account target = new Account(1000);
+
+            Thread[] adders = new Thread[3];
+            Thread[] removers = new Thread[3];
+            for (int i = 0; i < adders.Length; i++)
+            {
+                adders[i] = new Thread(() =>
+                {
+                    for (int n = 0; n < 100; n++)
+                    {
+                        target.Credit(100);
+                    }
+                });
+            }
+            for (int i = 0; i < removers.Length; i++)
+            {
+                removers[i] = new Thread(() =>
+                {
+                    for (int n = 0; n < 100; n++)
+                    {
+                        target.Debit(100);
+                    }
+                });
+            }
+
+            for (int i = 0; i < adders.Length; i++)
+            {
+                adders[i].Start();
+            }
+            for (int i = 0; i < removers.Length; i++)
+            {
+                removers[i].Start();
+            }
+
+            for (int i = 0; i < adders.Length; i++)
+            {
+                adders[i].Join();
+            }
+            for (int i = 0; i < removers.Length; i++)
+            {
+                removers[i].Join();
+            }
+
+            Console.WriteLine($"Final balance is ... {target.Balance}");
         }
     }
 }
---
+++ ParallelExamples/ThreadExamples/Account.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParallelExamples.ThreadExamples
+{
+    public class Account
+    {
+        //private readonly object balanceLock = new object();
+        private decimal balance;
+        public decimal Balance { get => balance; }
+
+        public Account(decimal initialBalance)
+        {
+            balance = initialBalance;
+        }
+
+        public decimal Debit(decimal amount)
+        {
+           // lock (balanceLock)
+            //{
+                if (balance >= amount)
+                {
+                    Console.WriteLine($"Balance before debit :{balance,5}");
+                    Console.WriteLine($"Amount to remove     :{amount,5}");
+                    balance = balance - amount;
+                    Console.WriteLine($"Balance after debit  :{balance,5}");
+                    return amount;
+                }
+                else
+                {
+                    return 0;
+                }
+           // }
+        }
+
+        public void Credit(decimal amount)
+        {
+          //  lock (balanceLock)
+           // {
+                Console.WriteLine($"Balance before credit:{balance,5}");
+                Console.WriteLine($"Amount to add        :{amount,5}");
+                balance = balance + amount;
+                Console.WriteLine($"Balance after credit :{balance,5}");
+           // }
+        }
+    }
+}
